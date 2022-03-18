eb7ac147e1c0c02f81f1458d7fd88c2b92e33a6b
Richard <rsarubbi@gmail.com>
2022-03-18 18:29:17 -0300

    Alta de proyecto al repo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ricardo Sarubbi Cufré
         </a>
       </header>
     </div>
