6598e3f6b0846e96ef58f5c0a7b11ceba19e57f8
Richard <rsarubbi@gmail.com>
2022-04-11 17:42:49 -0300

    Agregado de rutas

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from 'react'
 import { useState } from 'react'
 import './App.css';
-import NavBar from './components/NavBar/NavBar'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import Counter from './components/Counter/Counter';
+import NavBar from './components/NavBar/NavBar'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 
 const App = () => {
-  const props = {
-    title: 'Bienvenidos a la Tienda Virtual',
-    props:'2'
-  }
-
-  const [show, setShow] = useState(true)
-
-  const handleOnAdd = (quantity) => {
-    console.log(`se agregaron ${quantity} productos`)
-  }
 
   return (
       <div className="App">
-        <NavBar />
-        <ItemListContainer {...props} />
-
-        { show ? <Counter initial={0} stock={10} onAdd={handleOnAdd}/> : null }
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} />
+            <Route path='/category/:categoryId' element={<ItemListContainer />} />
+            <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+            <Route path='*' element={<h1>NOT FOUND 404</h1>}/>
+          </Routes>
+        </BrowserRouter>
+        
       </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,39 @@
 import './NavBar.css'
 import CartWidget from '../CartWidget/CartWidget'
+import { Link, NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { getCategories } from '../../asyncmock'
 
 const NavBar = () => {
-    return(
-        <nav className="NavBar">
-            <div>
-                <h3>Richard's Apparels</h3>
-            </div>
-            <div>
-                <button style={{ borderRadius: '10%',backgroundColor: 'darkblue',color: '#FFFFFF', marginRight: 15}}>Zapatillas</button>
-                <button style={{ borderRadius: '10%',backgroundColor: 'darkblue',color: '#FFFFFF', marginRight: 15}}>Pantalones</button>
-                <button style={{ borderRadius: '10%',backgroundColor: 'darkblue',color: '#FFFFFF', marginRight: 15}}>Camisas</button>
-                <button style={{ borderRadius: '10%',backgroundColor: 'darkblue',color: '#FFFFFF', marginRight: 15}}>Remeras</button>
-                <button style={{ borderRadius: '10%',backgroundColor: 'darkblue',color: '#FFFFFF', marginRight: 15}}>Vestidos</button>
-                <button style={{ borderRadius: '10%',backgroundColor: 'darkblue',color: '#FFFFFF', marginRight: 15}}>Mallas</button>
-            </div>
-            <div>
-                <CartWidget />
-            </div>
-            
-        </nav>
-    )
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then(categories => {
+      setCategories(categories)
+    })
+  }, [])
+
+  return (
+      <nav className="NavBar" >
+        <Link to='/'>
+            <h3>Richard's MarketPlace</h3>
+        </Link>
+        <div className="Categories">
+          {/* <button className='Option'>Celular</button>
+          <button className='Option'>Tablet</button>
+          <button className='Option'>Notebook</button> */}
+          {/* <Link to='/list' className='Option'>List</Link>
+          <Link to='/detail' className='Option'>Detail</Link> */}
+          {/* <NavLink to='/list' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>List</NavLink>
+          <NavLink to='/detail' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Detail</NavLink>
+          <NavLink to='/nosedonde' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>OTRO</NavLink> */}
+          { categories.map(cat => <NavLink key={cat.id} to={`/category/${cat.id}`}
+            className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}
+          >{cat.description}</NavLink>)}
+        </div>
+        <CartWidget />
+      </nav>
+  )
 }
 
 export default NavBar
