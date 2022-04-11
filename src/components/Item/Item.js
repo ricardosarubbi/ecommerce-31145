a358a1efe09600edf475f450690b17a1687bc0f0
Richard <rsarubbi@gmail.com>
2022-04-11 19:25:32 -0300

    Agregado de estilos y detalle

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,12 +1,27 @@
-const Item = ({ name, img}) => {
-    return(
-        <section>
+import './Item.css'
+import { Link } from 'react-router-dom'
+
+const Item = ({id, name, img, price}) => {
+   
+    return (
+        <article className="CardItem">
+            <header className="Header">
+                <h2 className="ItemHeader">
+                    {name}
+                </h2>
+            </header>
             <picture>
-                <img src={img} alt={name}/>
+                <img src={img} alt={name} className="ItemImg"/>
             </picture>
-            <h3>{name}</h3>
-            <button>Ver Detalle</button>
-        </section>
+            <section>
+                <p className="Info">
+                    Precio: ${price}
+                </p>
+            </section>           
+            <footer className='ItemFooter'>
+                <Link to={`/detail/${id}`} className='Option'>Ver detalle</Link>
+            </footer>
+        </article>
     )
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -19,14 +19,6 @@ const NavBar = () => {
             <h3>Richard's MarketPlace</h3>
         </Link>
         <div className="Categories">
-          {/* <button className='Option'>Celular</button>
-          <button className='Option'>Tablet</button>
-          <button className='Option'>Notebook</button> */}
-          {/* <Link to='/list' className='Option'>List</Link>
-          <Link to='/detail' className='Option'>Detail</Link> */}
-          {/* <NavLink to='/list' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>List</NavLink>
-          <NavLink to='/detail' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Detail</NavLink>
-          <NavLink to='/nosedonde' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>OTRO</NavLink> */}
           { categories.map(cat => <NavLink key={cat.id} to={`/category/${cat.id}`}
             className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}
           >{cat.description}</NavLink>)}
