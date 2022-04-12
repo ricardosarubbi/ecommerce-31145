80496e62938fa46a7232a0954fd53679392d8e99
Richard <rsarubbi@gmail.com>
2022-04-11 22:34:24 -0300

    Arreglo de filtos

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,21 +1,24 @@
 import { useState, useEffect } from 'react'
 import { getProducts } from '../../asyncmock'
 import ItemList from '../ItemList/ItemList'
+import { useParams } from 'react-router-dom'
 
 const ItemListContainer = (props) => {
     const [products, setProducts] = useState([])
 
+    const { categoryId } = useParams()
+
     useEffect(() => {
-        getProducts().then(prods => {
+        getProducts(categoryId).then(prods => {
             setProducts(prods)
         }).catch(error => {
             console.log(error)
         })
-    }, [])
+    }, [categoryId])
 
     return(
         <div>
-            <h1>Listado de Productos</h1>
+            <h1>{props.greeting}</h1>
             <ItemList products={products}/>
         </div>
     )
