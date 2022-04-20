575fc7dba9ed990b3c49890d3056d1c27279be4b
Richard <rsarubbi@gmail.com>
2022-04-20 20:09:18 -0300

    Sumar al carrito

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { useState } from 'react'
 import './App.css';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Form from './components/Form/Form';
+import { CartContextProvider } from './context/CartContext';
+import Cart from './components/Cart/Cart';
+import { NotificationProvider } from './notification/Notification';
 
 const App = () => {
-
   return (
       <div className="App">
-        <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route path='/' element={<ItemListContainer />} />
-            <Route path='/category/:categoryId' element={<ItemListContainer />} />
-            <Route path='/detail/:productId' element={<ItemDetailContainer />} />
-            <Route path='*' element={<h1>NOT FOUND 404</h1>}/>
-          </Routes>
-        </BrowserRouter>
-        
+        <NotificationProvider>
+          <CartContextProvider>
+            <BrowserRouter>
+              <NavBar />
+              <Routes>
+                <Route path='*' element={<h1>NOT FOUND 404</h1>}/>
+                <Route path='/' element={<ItemListContainer />} />
+                <Route path='/category/:categoryId' element={<ItemListContainer />} />
+                <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+                <Route path='/form' element={<Form />} />
+                <Route path='/cart' element={<Cart />} />
+              </Routes>
+            </BrowserRouter> 
+          </CartContextProvider>
+        </NotificationProvider>
       </div>
   );
 }
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,12 +1,18 @@
 import './CartWidget.css'
+import { useContext } from 'react';
+import CartContext from '../../context/CartContext';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
+
+    const { getQuantity } = useContext(CartContext)
+
     return(
-        <div className="CartWidget">
-            <img src='./images/cart3.svg' alt='cart' className='CartImg' style={{ width: 25, marginRight: 15}}/>
-            0
-        </div>
-    )
+        <Link to='/cart' className="CartWidget">
+            <img src="/images/cart.svg" alt='cart' className='CartImg'/>
+            { getQuantity() }
+        </Link>
+    );
 }
 
 export default CartWidget
